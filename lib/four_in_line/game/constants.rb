745002f52df8452da50dbc2a90b2131f65cba5a8
Juan M. Furattini <juan.furattini@gmail.com>
2015-11-11 11:11:30 -0300

    Four In Line

--- lib/four_in_line/game/constants.rb
+++ lib/four_in_line/game/constants.rb
@@ -6,7 +6,6 @@ module FourInLine
       EMPTY_BLOCK = 0
       PLAYER_1 = 1
       PLAYER_2 = 2
-			MESSAGE = { EMPTY_BLOCK => "DRAW", PLAYER_1 => "PLAYER 1", PLAYER_2 => "PLAYER 2"}
 		end
 	end
 end
